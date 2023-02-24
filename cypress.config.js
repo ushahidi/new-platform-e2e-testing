dd529a467329073b4824948ac907c27c34dfc5c3
AmTryingMyBest <w.obadha@gmail.com>
2023-02-24 13:01:58 +0300

    automate login process

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 960,
+  defaultCommandTimeout: 20000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
