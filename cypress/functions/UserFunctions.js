73cf7b24ae48f2b59110eb7eb9f71c77f3471296
Walter Loso <loso@Walters-MacBook-Pro-3.local>
2023-03-24 15:13:06 +0300

    delete auto created user

--- cypress/functions/UserFunctions.js
+++ cypress/functions/UserFunctions.js
@@ -1,13 +1,13 @@
 import UserLocators from "../locators/UserLocators";
 
 class UserFunctions {
-  open_user_page() {
+  open_user_page_steps() {
     cy.wait(2000);
     cy.get(UserLocators.stngsBtn).click();
     cy.get(UserLocators.usersBtn).click();
   }
 
-  add_user(userName, userEmail, pwd) {
+  add_user_steps(userName, userEmail, pwd) {
     cy.get(UserLocators.addUserBtn).click();
     cy.get(UserLocators.nameField).type(userName);
     cy.get(UserLocators.emailField).type(userEmail);
@@ -17,13 +17,32 @@ class UserFunctions {
     cy.get(UserLocators.saveBtn).click();
   }
 
-  navigate_and_add_user() {
-    this.open_user_page();
-    this.add_user(
+  delete_user_steps() {
+    //type search term in bits for search to actually happen
+    //typing all at once, search doesn't happen
+    cy.get(".p-inputtext").type("Aut");
+    cy.get(".p-inputtext").type("o");
+
+    cy.get(UserLocators.bulkActionsBtn).click();
+    cy.wait(1000);
+    //add unique selector for checkbox
+    cy.get(
+      ".p-selectable-row > :nth-child(1) > .p-element > .p-checkbox > .p-checkbox-box > .p-checkbox-icon"
+    ).click({ force: true });
+    cy.get(UserLocators.deleteUsersBtn).click();
+    cy.get(UserLocators.deleteUserConfirmBtn).click();
+  }
+
+  add_user() {
+    this.open_user_page_steps();
+    this.add_user_steps(
       "Auto User",
       "autouser@ushahidi.com",
       Cypress.env("ush_user_pwd")
     );
+  }
+  delete_user() {
+    this.delete_user_steps();
   }
 }
 
